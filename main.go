ba4c6bc8daf7e5e4112b9afa583ba531ca31293f
Nolan Kovacik <noltron000@gmail.com>
2019-03-07 11:55:41 -0800

    💼 working example

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":8080", "listen address")
+	dir    = flag.String("dir", ".", "directory to serve")
+)
+
 func main() {
-	println("Hello World")
+	flag.Parse()
+	log.Printf("listening on %q...", *listen)
+	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
 }
